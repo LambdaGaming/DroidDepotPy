25f0f4a7a313e8004e624ac86244534b9ee99864
LambdaGaming <starwars18m@gmail.com>
2021-07-16 19:58:15 -0400

    Fixed timeout and bytearray issues

--- droid.py
+++ droid.py
@@ -5,50 +5,46 @@ SERVICE_UUID = "09b600a0-3e42-41fc-b474-e9c0c8f0c801"
 NOTIFY_UUID = "09b600b0-3e42-41fc-b474-e9c0c8f0c801"
 WRITE_UUID = "09b600b1-3e42-41fc-b474-e9c0c8f0c801"
 
-def convertBytes( bytes ):
-	return bytearray( bytes )
-
 async def run():
 	droidCount = 0
 	droid = None
-	devices = await BleakScanner.discover()
+	devices = await BleakScanner.discover( timeout = 10 )
 	for d in devices:
 		if d.name == "DROID":
 			droid = d
 			droidCount += 1
-	if droidCount > 1:
-		print( "Found more than one droid. Using last one in the list." )
-	if droidCount == 0:
+	if droidCount > 0:
+		print( "Droid found! It's address is " + d.address )
+	else:
 		print( "No droids detected. Aborting." )
 		return
+	if droidCount > 1:
+		print( "Found more than one droid. Using last one in the list." )
 	if droid is not None:
 		async with BleakClient( droid.address ) as client:
 			try:
-				print( "Attempting to connect to droid..." )
-				connected = await client.connect()
-				if connected:
-					print( "Connection established successfully! Writing initial messages..." )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'222001' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'222001' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'222001' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'222001' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'27420f4444001f00' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'27420f4444001802' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'27420f4444001f00' )
-					await asyncio.sleep( 1.5 )
-					await client.write_gatt_char( WRITE_UUID, b'27420f4444001802' )
-					await asyncio.sleep( 2 )
+				print( "Connecting to droid..." )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001f00" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001802" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001f00" ) )
+				await asyncio.sleep( 1.5 )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001802" ) )
+				await asyncio.sleep( 2 )
 
-					print( "Successfully wrote initial messages! Attempting to play sound..." )
-					await client.write_gatt_char( WRITE_UUID, b'2742 0F44 4400 1803' )
-					print( "Successfully played sound!" )
+				print( "Successfully wrote initial messages! Attempting to play sound..." )
+				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2742 0F44 4400 1803" ) )
+				print( "Successfully played sound!" )
 			except KeyboardInterrupt:
 				print( "Program stopped by user." )
 			except BaseException as e:
