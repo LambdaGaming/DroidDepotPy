91c1fe87ac3aa2e9be16c0acd9dfe63cc8084d7e
LambdaGaming <starwars18m@gmail.com>
2021-07-17 14:50:23 -0400

    Added turn left and turn right functions

--- droid.py
+++ droid.py
@@ -12,20 +12,47 @@ WRITE_UUID = "09b600b1-3e42-41fc-b474-e9c0c8f0c801"
 # Instance of the droid connection
 CLIENT = None
 
+# It seems that different personalities use different sequences,
+# so some sequences won't make the droid play a sound
 async def playRandomSound():
 	global CLIENT
 	soundBanks = ["0", "1", "2", "3", "4", "5", "6", "7", "A"]
 	soundCodes = ["0", "1", "2", "3", "4"]
 	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2742 0F44 4400 1F0" + random.choice( soundBanks ) ) )
 	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2742 0F44 4400 180" + random.choice( soundCodes ) ) )
+	print( "Playing random sound..." )
+
+async def turnRight():
+	global CLIENT
+	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2942 0546 00FF 012C 0000" ) )
+	print( "Turning right..." )
+
+async def turnLeft():
+	global CLIENT
+	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2942 0546 01FF 012C 0000" ) )
+	print( "Turning left..." )
+
+async def disableMotors():
+	global CLIENT
+	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2942 0546 0000 012C 0000" ) )
+	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2942 0546 0100 012C 0000" ) )
 
 async def keyPress():
-	while True:
+	while True: #TODO: Make this less CPU-intensive
 		if keyboard.is_pressed( '1' ):
 			await playRandomSound()
 			await asyncio.sleep( 1 )
+		elif keyboard.is_pressed( 'd' ):
+			await turnRight()
+			await asyncio.sleep( 0.5 )
+			await disableMotors()
+		elif keyboard.is_pressed( 'a' ):
+			await turnLeft()
+			await asyncio.sleep( 0.5 )
+			await disableMotors()
 
 async def connect():
+	print( "Scanning for droids..." )
 	droidCount = 0
 	droid = None
 	devices = await BleakScanner.discover( timeout = 10 )
