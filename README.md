fe10f4eb3563f57f55b076f7119697a7ac3874a5
LambdaGaming <starwars18m@gmail.com>
2021-07-17 02:31:44 -0400

    Added random sound function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DroidDepotPy
- Python program to control Droid Depot droids.
+ Python program to control Droid Depot droids. Based on [rileytb's web-based droid controller](https://github.com/rileytb/droid-depot-bt) as well as information gathered from [SA-45.](https://github.com/siddacious/SA-45)
--- droid.py
+++ droid.py
@@ -1,11 +1,31 @@
 import asyncio
+import keyboard
+import random
 from bleak import BleakClient, BleakScanner
 
+# UUIDs for the service, notifications, and writing
+# Currently, only the write UUID is used
 SERVICE_UUID = "09b600a0-3e42-41fc-b474-e9c0c8f0c801"
 NOTIFY_UUID = "09b600b0-3e42-41fc-b474-e9c0c8f0c801"
 WRITE_UUID = "09b600b1-3e42-41fc-b474-e9c0c8f0c801"
 
-async def run():
+# Instance of the droid connection
+CLIENT = None
+
+async def playRandomSound():
+	global CLIENT
+	soundBanks = ["0", "1", "2", "3", "4", "5", "6", "7", "A"]
+	soundCodes = ["0", "1", "2", "3", "4"]
+	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2742 0F44 4400 1F0" + random.choice( soundBanks ) ) )
+	await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2742 0F44 4400 180" + random.choice( soundCodes ) ) )
+
+async def keyPress():
+	while True:
+		if keyboard.is_pressed( '1' ):
+			await playRandomSound()
+			await asyncio.sleep( 1 )
+
+async def connect():
 	droidCount = 0
 	droid = None
 	devices = await BleakScanner.discover( timeout = 10 )
@@ -21,36 +41,37 @@ async def run():
 	if droidCount > 1:
 		print( "Found more than one droid. Using last one in the list." )
 	if droid is not None:
-		async with BleakClient( droid.address ) as client:
+		global CLIENT
+		async with BleakClient( droid.address ) as CLIENT:
 			try:
 				print( "Connecting to droid..." )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "222001" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001f00" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001f00" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001802" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001802" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001f00" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001f00" ) )
 				await asyncio.sleep( 1.5 )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001802" ) )
+				await CLIENT.write_gatt_char( WRITE_UUID, bytearray.fromhex( "27420f4444001802" ) )
 				await asyncio.sleep( 2 )
-
-				print( "Successfully wrote initial messages! Attempting to play sound..." )
-				await client.write_gatt_char( WRITE_UUID, bytearray.fromhex( "2742 0F44 4400 1803" ) )
-				print( "Successfully played sound!" )
+				print( "Connection to droid established! Droid is ready to accept inputs!" )
+				await keyPress()
 			except KeyboardInterrupt:
 				print( "Program stopped by user." )
 			except BaseException as e:
 				print( "ERROR: {0}".format( e ) )
 			finally:
-				await client.disconnect()
+				await CLIENT.disconnect()
+	else:
+		print( "ERROR: Droid was found but it's connection somehow isn't valid." )
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete( run() )
+loop.run_until_complete( connect() )
